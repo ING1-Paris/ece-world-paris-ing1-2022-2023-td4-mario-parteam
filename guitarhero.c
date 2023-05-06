2ee07393f704d4a4584caab79c1600aaf14320cc
Scotch <raphael.duport@gmail.com>
2023-05-06 23:52:01 +0200

    Debut code du minijeu guitare hero, detection du score et initialisation des fonctions + algorithme du jeu
    Reste à :
    -Animation et création des sprites
    -créer la bitmap de jeu
    -créer le menu
    -créer le "main" du jeu guitarehero

--- guitarhero.c
+++ guitarhero.c
@@ -3,3 +3,45 @@
 //
 
 #include "guitarhero.h"
+
+void initBoutons(t_boutons *boutons){
+    //load les bitmaps
+    boutons->clavier[0]=KEY_A;
+    boutons->clavier[1]=KEY_Z;
+    boutons->clavier[2]=KEY_E;
+    boutons->clavier[3]=KEY_R;
+    boutons->clavier[4]=KEY_T;
+    //déclarer la pos des boutons
+    boutons->color[0]=makecol(0,255,0); //VERT
+    boutons->color[0]=makecol(255,0,0); //ROUGE
+    boutons->color[0]=makecol(255,255,0); //JAUNE
+    boutons->color[0]=makecol(0,0,255); //BLEU
+    boutons->color[0]=makecol(255,128,0); //ORANGE
+    //à changer quand les bitmap seront faites
+}
+
+void detectScore(t_score *score, t_boutons *boutons,BITMAP *page){
+    int press;
+    int ybuffer[5];
+    press=0;
+    for(int i=0;i<5;i++){
+        ybuffer[i]=boutons->yboutons[i];
+        for(int j=-ECARTPARFAIT/2;j<ECARTPARFAIT;j++){
+            ybuffer[i]+=j;
+            if(keypressed()==boutons->clavier[i] && getpixel(page,boutons->xboutons[i],ybuffer[i])==boutons->color[i]){
+                press=PARFAIT;
+            }else
+                for(int k=-ECARTMOYEN/2;k<ECARTMOYEN;k++){
+                    ybuffer[i]+=k;
+                    if(keypressed()==boutons->clavier[i] && getpixel(page,boutons->xboutons[i],ybuffer[i])==boutons->color[i]){
+                        press=MOYEN;
+                    }else
+                        press=RATE;
+                }
+        }
+    }
+    score->score+=press;
+}
+
+
+
--- guitarhero.h
+++ guitarhero.h
@@ -4,5 +4,36 @@
 
 #ifndef MARIO_PARTEAM_V1_GUITARHERO_H
 #define MARIO_PARTEAM_V1_GUITARHERO_H
+#include "generalfunctions.h"
+#define PARFAIT 100
+#define MOYEN 50
+#define RATE 0
+#define ANIMATION 5
+#define ECARTPARFAIT 10
+#define ECARTMOYEN 100
+typedef struct boutons{
+    BITMAP *vert[ANIMATION];    //0     a
+    BITMAP *rouge[ANIMATION];   //1     z
+    BITMAP *jaune[ANIMATION];   //2     e
+    BITMAP *bleu[ANIMATION];    //3     r
+    BITMAP *orange[ANIMATION];  //4     t
+    int clavier[5];
+    int xboutons[5];
+    int yboutons[5];
+    int color[5];
+}t_boutons;
+
+typedef struct score{
+    long int score;
+    //tab de score ?
+    short int combo;
+}t_score;
+
+void initBoutons(t_boutons *boutons);
+void menuGh();
+void animationBoutons(t_boutons *boutons);
+void detectScore(t_score *score, t_boutons *boutons,BITMAP *page); // appeler animationBoutons dans cette fonction
+void guitareHero(t_score *score, t_boutons *boutons); //fonction final du jeu, avec BITMAP *stage, menu.... ect
+
 
 #endif //MARIO_PARTEAM_V1_GUITARHERO_H
